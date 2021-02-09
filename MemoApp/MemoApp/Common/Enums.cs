1f526b1d7caab135a0a09d1c43eef00fb4b94453
natasajevtic <nataly.jevtic.95@gmail.com>
2021-02-09 09:07:33 +0100

    Add wrapper classes

---
+++ MemoApp/MemoApp/Common/Enums.cs
@@ -0,0 +1,10 @@
+﻿namespace MemoApp.Common
+{
+    public enum StatusEnum
+    {        
+        Active = 1,
+        Deleted = 2,
+        Error = 3,
+        Succeeded = 4
+    }
+}
---
+++ MemoApp/MemoApp/Common/IResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace MemoApp.Common
+{
+    public interface IResponse<T>
+    {
+        HttpStatusCode StatusCode { get; set; }
+        T Content { get; set; }
+        List<ErrorResult> ErrorMessage { get; set; }
+    }
+}
