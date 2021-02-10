6077fad98bc550628029b942a0a6499325a5361c
natasajevtic <nataly.jevtic.95@gmail.com>
2021-02-10 14:34:33 +0100

    Add service for data access

---
+++ MemoApp/MemoApp/Services/IMemoService.cs
@@ -0,0 +1,10 @@
+﻿using MemoApp.Common;
+using MemoApp.Data;
+
+namespace MemoApp.Services
+{
+    public interface IMemoService
+    {
+        IFeedback<long> AddMemo(Memo memo);        
+    }
+}
---
+++ MemoApp/MemoApp/Services/MemoService.cs
@@ -0,0 +1,51 @@
+﻿using MemoApp.Common;
+using MemoApp.Data;
+using Serilog;
+using System;
+
+namespace MemoApp.Services
+{
+    public class MemoService : IMemoService
+    {
+        private readonly MemoEntities _entities;
+
+        public MemoService(MemoEntities entities)
+        {
+            _entities = entities;
+        }
+
+        public IFeedback<long> AddMemo(Memo memo)
+        {
+            var feedback = new Feedback<long>();
+            try
+            {
+                _entities.Tags.AddRange(memo.Tags);
+                _entities.Memos.Add(memo);
+
+                if (Commit().Value)
+                {
+                    feedback.Value = memo.Id;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Failed to add a new memo: {ex.GetBaseException().Message}");
+            }
+            return feedback;
+        }        
+
+        public IResult<bool> Commit()
+        {
+            var result = new Result<bool>();
+            try
+            {
+                result.Value = _entities.SaveChanges() > 0;
+            }
+            catch (Exception ex)
+            {                
+                Log.Error($"Failed to save changes to database: {ex.GetBaseException().Message}");
+            }
+            return result;
+        }
+    }
+}
